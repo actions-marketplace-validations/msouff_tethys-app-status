42e01fcbdeeacd35f459514b3aa54ae4bb2d778f
msouff <17328135+msouff@users.noreply.github.com>
2021-05-25 18:41:58 -0600

    make sure installComplete key exists

--- tethys_app_status.py
+++ tethys_app_status.py
@@ -37,37 +37,43 @@ def status_check() -> str:
 
     data = json.loads(res.content)
 
-    if not data['installComplete'] and check_count <= 12:  # first 4 minutes
-        time.sleep(20)
-        check_count += 1
-        c_print(data['status'], blue_style)
-        status_check()
-    elif not data['installComplete'] and 12 < check_count <= 15:  # next 3 minutes
-        c_print('App installation is taking longer than expected.', blue_style)
-        time.sleep(60)
-        check_count += 1
-        c_print(data['status'], blue_style)
-        status_check()
-    elif not data['installComplete'] and 15 < check_count == 16:  # next 3 minutes
-        c_print('App installation is taking longer than expected.', blue_style)
-        time.sleep(120)
-        check_count += 1
-        c_print(data['status'], blue_style)
-        status_check()
-    elif not data['installComplete'] and check_count > 16:  # print logs
-        c_print('App installation took too long. See logs below.', orange_style)
-        logs_base_url = os.path.join(os.path.dirname(base_url), "logs")
-        logs_res = requests.get(logs_base_url, params=request_params, headers=request_headers)
+    if 'installComplete' in data.keys():
+        if not data['installComplete'] and check_count <= 12:  # first 4 minutes
+            time.sleep(20)
+            check_count += 1
+            c_print(data['status'], blue_style)
+            status_check()
+        elif not data['installComplete'] and 12 < check_count <= 15:  # next 3 minutes
+            c_print('App installation is taking longer than expected.', blue_style)
+            time.sleep(60)
+            check_count += 1
+            c_print(data['status'], blue_style)
+            status_check()
+        elif not data['installComplete'] and 15 < check_count == 16:  # next 3 minutes
+            c_print('App installation is taking longer than expected.', blue_style)
+            time.sleep(120)
+            check_count += 1
+            c_print(data['status'], blue_style)
+            status_check()
+        elif not data['installComplete'] and check_count > 16:  # print logs
+            c_print('App installation took too long. See logs below.', orange_style)
+            logs_base_url = os.path.join(os.path.dirname(base_url), "logs")
+            logs_res = requests.get(logs_base_url, params=request_params, headers=request_headers)
 
-        log_data = logs_res.content
-        c_print(log_data, orange_style)
-        c_print('\nRESULT: Failed', orange_style)
-    elif data['installComplete']:  # successful installation
-        c_print(data['status'], green_style)
-        c_print(f'Install Completed Time: {data["installCompletedTime"]}', green_style)
-        c_print('\nRESULT: Success', green_style)
-    else:  # installation failed
-        c_print('\nRESULT: Failed', red_style)
+            log_data = logs_res.content
+            c_print(log_data, orange_style)
+            c_print('\nRESULT: Failed', orange_style)
+        elif data['installComplete']:  # successful installation
+            c_print(data['status'], green_style)
+            c_print(f'Install Completed Time: {data["installCompletedTime"]}', green_style)
+            c_print('\nRESULT: Success', green_style)
+        else:  # installation failed
+            c_print('\nRESULT: Failed', red_style)
+    else:
+        time.sleep(30)
+        check_count += 1
+        if check_count < 10:
+            status_check()
 
 
 def main() -> str:
